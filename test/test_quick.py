bf91aa7f889ca50ed8600a1ad556e21ac6c805ab
JengCharat <zazajeng401@gmail.com>
2025-12-10 21:26:43 +0700

    quick sort

--- test/test_quick.py
+++ test/test_quick.py
@@ -1,25 +1,25 @@
 import unittest
-from Project.main import bubble_sort
+from Project.main import quick_sort
 
-class TestBubbleSort(unittest.TestCase):
+class TestQuickSort(unittest.TestCase):
 
     def test_basic_sort(self):
-        self.assertEqual(bubble_sort([5, 3, 1, 4, 2]), [1, 2, 3, 4, 5])
+        self.assertEqual(quick_sort([5, 3, 1, 4, 2]), [1, 2, 3, 4, 5])
 
     def test_already_sorted(self):
-        self.assertEqual(bubble_sort([1, 2, 3, 4]), [1, 2, 3, 4])
+        self.assertEqual(quick_sort([1, 2, 3, 4]), [1, 2, 3, 4])
 
     def test_reverse_order(self):
-        self.assertEqual(bubble_sort([4, 3, 2, 1]), [1, 2, 3, 4])
+        self.assertEqual(quick_sort([4, 3, 2, 1]), [1, 2, 3, 4])
 
     def test_duplicates(self):
-        self.assertEqual(bubble_sort([3, 1, 2, 3]), [1, 2, 3, 3])
+        self.assertEqual(quick_sort([3, 1, 2, 3]), [1, 2, 3, 3])
 
     def test_single_element(self):
-        self.assertEqual(bubble_sort([10]), [10])
+        self.assertEqual(quick_sort([10]), [10])
 
     def test_empty(self):
-        self.assertEqual(bubble_sort([]), [])
+        self.assertEqual(quick_sort([]), [])
 
 if __name__ == "__main__":
     unittest.main()
